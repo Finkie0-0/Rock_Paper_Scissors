211cf78c1e504683fa6cb5aca9063d51735fa06b
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-08-25 01:24:13 +0200

    Create the computer's choice function that will randomly choice between rock, paper or scissors

--- script.js
+++ script.js
@@ -2,9 +2,27 @@
 // use the math.random () function
 // Make the scope of math.random() function shrink to accommodate the three outputs
 
-// If math.floor(math.random() * 4) === 1;
+// If math.floor(math.random() * 4) === 0;
     // return Rock
-// else if math.floor(math.random() *4 == 2);
+// else if math.floor(math.random() *4 == 1);
     // return Paper
 // else
     // return Scissors
+
+// Start coding the computer's choice function
+
+function getComputerChoice(){
+    let move = Math.floor(Math.random() * 3);
+
+    if (move === 0){
+        return (`Rock`);
+    } else if( move === 1){
+        return (`Paper`);
+    }else{
+        return (`Scissors`);
+    }
+    
+    
+}
+
+console.log(getComputerChoice());
