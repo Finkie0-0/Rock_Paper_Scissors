3337ec46845ee1bfd8ad54e282df8b68fd3516be
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-08-25 01:10:31 +0200

    Plan the computer's choice's random output

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello Arnold!");
+// Create a function to get the computer's choice; Represented a Rock, Paper and Scissors
+// use the math.random () function
+// Make the scope of math.random() function shrink to accommodate the three outputs
+
+// If math.floor(math.random() * 4) === 1;
+    // return Rock
+// else if math.floor(math.random() *4 == 2);
+    // return Paper
+// else
+    // return Scissors
